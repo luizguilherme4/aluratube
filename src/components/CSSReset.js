edf76e73984625f4a862b2f10fffd7394b4ecbef
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2022-11-12 12:48:59 -0300

    add darkmode

--- src/components/CSSReset.js
+++ src/components/CSSReset.js
@@ -9,30 +9,32 @@ export const CSSReset = createGlobalStyle`
   }
   body {
     font-family: sans-serif;
+    background-color: ${({ theme }) => theme.backgroundBase};
+    color: ${({ theme }) => theme.textColorBase};
   }
   /* NextJS */
   html {
-    display: flex;
-    flex-direction: column;
-    min-height: 100%;
-  }
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+}
   body {
-    display: flex;
-    flex: 1;
-  }
-  #__next {
-    display: flex;
-    flex: 1;
-  }
-  /* Globals */
-  button,
+  display: flex;
+  flex: 1;
+}
+#__next {
+  display: flex;
+  flex: 1;
+}
+/* Globals */
+button,
   a {
-    text-decoration: none;
-    opacity: 1;
-    transition: .3s;
+  text-decoration: none;
+  opacity: 1;
+  transition: .3s;
     &:hover,
     &:focus {
-      opacity: .5;
-    }
+    opacity: .5;
   }
+}
 `;
